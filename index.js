e8f4766ac13731d6f3e0a6270bc6434d8eed7808
taria <taria.ki4m@gmail.com>
2024-04-10 02:41:25 +0000

    supported height

--- index.js
+++ index.js
@@ -48,7 +48,12 @@ module.exports = class extends mofron.class.Component {
             this.text(
 	        Text,
 		{
-		    size:'0.25rem', style:{ 'text-align':'center' },
+		    size:'0.25rem',
+		    style:{
+		        'text-align':  'center',
+			'white-space': 'nowrap',
+			'padding':     '0.1rem'
+                    },
 		}
             );
 	    this.frame(
@@ -111,7 +116,7 @@ module.exports = class extends mofron.class.Component {
 
             text.config({
 	        text: prm,
-		style: { 'width': (0.25*(prm.length+offset))+'rem'  }
+		//style: { 'width': (0.25*(prm.length+offset))+'rem'  }
             });
 	    text.style()
             
@@ -202,5 +207,27 @@ module.exports = class extends mofron.class.Component {
         }
     }
 
+    height (prm) {
+        try {
+            let frm_cnf = this.data('frame-cnf');
+            if (undefined === prm) {
+                return frm_cnf['height'];
+            }
+            frm_cnf['height'] = prm;
+            this.data('frame-cnf', frm_cnf);
+            
+	    let chd = this.child();
+            for (let cidx in chd) {
+                if (true !== comutl.isinc(chd[cidx],'Frame')) {
+                    continue;
+		}
+		chd[cidx].height(prm);
+		chd[cidx].child()[0].size(comutl.sizediff(prm,"0.06rem"));
+	    }
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
 }
 /* end of file */
